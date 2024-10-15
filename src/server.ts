7adbf7d8e8683a39fc1ab2f82460f816527fc529
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-15 19:30:39 +0600

    eslint configuration done

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ async function main() {
     
     await Mongoose.connect(config.db_Url as string);
 
+    
+
     const port = config.port;
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
