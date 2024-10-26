34e65fd0c9aa1b01d18197edb927277906a08279
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-27 00:01:54 +0600

    slot model created

---
+++ src/modules/slot/slot.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+import { ISlot } from "./slot.interface";
+
+const SlotSchema = new Schema<ISlot>(
+  {
+    service: {
+      type: String,
+      required: [true, "Service is required"],
+    },
+    date: {
+      type: String,
+      required: [true, "Date is required"],
+    },
+    startTime: {
+      type: String,
+      required: [true, "Start time is required"],
+    },
+    endTime: {
+      type: String,
+      required: [true, "End time is required"],
+    },
+    isBooked: {
+      type: String,
+      enum: ["available", "booked", "cancelled"],
+      default: "available",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<ISlot>("Slot", SlotSchema);
