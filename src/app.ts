814c7fa37e92631ee717464cb3b1cab6b68723d1
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-26 20:29:00 +0600

    mongodb connected

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
-import { MovieRoutes } from "./modules/movies/movie.route";
+// import { MovieRoutes } from "./modules/movies/movie.route";
 const app = express();
 
 // Parser
 app.use(express.json());
 
 // Routes
-app.use("/api/movies", MovieRoutes);
+// app.use("/api/movies", MovieRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
