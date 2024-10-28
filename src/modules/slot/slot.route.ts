42e1c04c3a39b70b473d0c89354f37b60ceb317f
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-28 16:48:55 +0600

    fixed some bugs

--- src/modules/slot/slot.route.ts
+++ src/modules/slot/slot.route.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 // Create Slot
 router.post("/", SlotControllers.createSlots);
 
-// Get all available slots for a service on a specified date
+// Get all available slots for a service on a specified date . 
 router.get("/availability", SlotControllers.getAvailableSlots);
 
 export const SlotRoutes = router;
