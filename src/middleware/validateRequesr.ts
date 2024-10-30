c9e82fb1fe5fc6e6ecfb659d4c29cbaa621f531e
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-30 20:49:01 +0600

    validate request middleware created

---
+++ src/middleware/validateRequesr.ts
@@ -0,0 +1,18 @@
+
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from "zod";
+import { catchAsync } from "../utils/catchAsync";
+
+const validateRequest = (schema: AnyZodObject) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const parsedBody = await schema.parseAsync({
+      body: req.body,
+    });
+
+    req.body = parsedBody.body;
+
+    next();
+  });
+};
+
+export default validateRequest;
